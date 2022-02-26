858aeacc6c165f0d28715f0bc5a949ac8dee77cc
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-26 06:30:23 -0300

    alt

--- Aula01/Aula01.cs
+++ Aula01/Aula01.cs
@@ -1,4 +1,5 @@
 // In C# programming language, a simple "hello world" program can be written by multiple ways. Let's see the top 4 ways to creat a simple C# example:
+//First example
 class Aula01
 {
     static void Main()
