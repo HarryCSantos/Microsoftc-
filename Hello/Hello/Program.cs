0d0e4f8329f30fc9309617065a4019be7fc8529f
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-02-27 11:23:44 -0300

    alt

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -5,7 +5,7 @@ namespace Hello
 {
     class Program
     {
-        static void Main(int[] args)
+        static void Main(int[])
         {
             //Console.WriteLine("Hello World!");
             Console.WriteLine(x + y);
