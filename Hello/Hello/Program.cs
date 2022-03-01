c3ce4bfaae94a18437f38fd39d73a7b5db2f3285
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-01 17:01:03 -0300

    alt

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class Hello
 {
     static void Main()
     {
-        int x = 4;
-        int y = 15;
-        int w = 34;
-        int z = x + y + w;
-        
+       
         Console.WriteLine(z);
     }
 }
