73a17a49c937748c8df3e3f031e8f214cd383e73
HarryCSantos <harry.santos@aluno.faculdadeimpacta.com.br>
2022-03-02 22:04:08 -0300

    alt

--- Aula01/Aula01.cs
+++ Aula01/Aula01.cs
@@ -1,13 +1,3 @@
 // In C# programming language, a simple "hello world" program can be written by multiple ways. Let's see some ways to creat a simple C# example:
 //First example
-using System;
-namespace ConsoleApplication1
-{ 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World !");
-    }
-}
-}
+
